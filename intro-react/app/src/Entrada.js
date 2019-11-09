3987783fd074acaa11d841ba718440001b56ccee
Hugo Marinho <hugoharan@gmail.com>
2019-11-08 21:59:04 -0300

    aulas de introducao ao react

---
+++ intro-react/app/src/Entrada.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Entrada extends Component {
+	render(){
+
+		return(
+			<div>
+				<label for="user"> Usuário</label>
+				<input type="text" name="user" value={this.props.valor} onChange={this.props.onInputChange} />
+				<button onClick={this.props.onButtonClick}>Procurar</button>
+
+			</div>
+		)
+	}
+}
+
+export default Entrada;
---
+++ intro-react/app/src/Usuario.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+class Usuario extends Component {
+	render(){
+		const {
+			avatar_url,
+			user,
+			name,
+			blog
+		} = this.props.info;
+		return(
+			<div>
+			 <img src="{this.props.info.avatar_url}" alt="avatar"/>
+			 <ul>
+			 	<li>Usuario: {user}</li>
+			 	<li>Nome: {name}</li>
+			 	<li>Blog: {blog}</li>
+			 	<li></li>
+			 </ul>
+			</div>
+		)
+	}
+}
+
+export default Usuario;
